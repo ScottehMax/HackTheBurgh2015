c15718c2bd8cbc7fc21d6c76029a5f870d1c2c30
Jordan Duncan <jordan@dogfi.sh>
2015-03-15 01:14:12 +0000

    All dat new jazz

--- api/controllers/InputController.js
+++ api/controllers/InputController.js
@@ -23,7 +23,39 @@ var getStuff = function (d) {
 		return false;
 	}
 
-}
+};
+
+var matchDown = function (text) {
+	var booRegex = /-down ([0-9]+)/i;
+	var matched = text.match(booRegex);
+
+	if (matched) {
+		if(matched[1].length == 4){
+			return matched[1];
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
+
+};
+
+var matchUp = function (text) {
+	var booRegex = /-up ([0-9]+)/i;
+	var matched = text.match(booRegex);
+
+	if (matched) {
+		if(matched[1].length == 4){
+			return matched[1];
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
+
+};
 
 module.exports = {
 	logText: function(req,res){
@@ -32,17 +64,53 @@ module.exports = {
 		var message = req.body.Body;
 		sails.log(sender + ": " + message);
 
-		request('https://api.spotify.com/v1/search?type=track&q=' + message, function (err, res, resp_body) {
-			if (!err && res.statusCode == 200) {
+		var isDown = matchDown(message);
+		var isUp = matchUp(message);
 
-				// for some reason, nobody told me the response would be a string
-				resp_body = JSON.parse(resp_body);
+		if(isDown){
+			var retObj = {'type':'down','code':isDown};
 
-				// NOTE: just logs for now
-				sails.log(getStuff(resp_body));
-			}
-		})
-		return true;
+			sails.sockets.blast(retObj);
+			return res.send(retObj);
+		} else if(isUp){
+			var retObj = {'type':'up','code':isUp};
+
+			sails.sockets.blast(retObj);
+			return res.send(retObj);
+		} else {
+			request('https://api.spotify.com/v1/search?type=track&q=' + message, function (err, resp, resp_body) {
+				if (!err && resp.statusCode == 200) {
+
+					// for some reason, nobody told me the response would be a string
+					resp_body = JSON.parse(resp_body);
+
+					sails.log(getStuff(resp_body));
+					sails.sockets.blast({'type':'add','data':getStuff(resp_body)});
+					return res.send({'type':'add','data':getStuff(resp_body)});
+				}
+			});
+		}
+	},
+	sample: function(req,res){
+		var searches = ['nothing more to say sophie', 'the kinks you really got me', 'lights fatherson'];
+		var count = 0;
+
+		function doReq(){
+			request('https://api.spotify.com/v1/search?type=track&q=' + searches[i], function (err, resp, resp_body) {
+				if (!err && resp.statusCode == 200) {
+
+					// for some reason, nobody told me the response would be a string
+					resp_body = JSON.parse(resp_body);
+
+					sails.log(getStuff(resp_body));
+					sails.sockets.blast({'type':'add','data':getStuff(resp_body)});
+
+					count++
+
+					doReq();
+				}
+			});
+		}
 	}
 };
 
